51efc8bc28e7ac99b634112c4b624524b0c14ad1
t-silvers <thomas.silvers@me.com>
2024-08-12 01:46:54 +0200

    add nfcore wrapper

---
+++ bio/nf-core/wrapper.py
@@ -0,0 +1,58 @@
+__author__ = "Johannes Köster"
+__copyright__ = "Copyright 2021, Johannes Köster"
+__email__ = "johannes.koester@uni-due.de"
+__license__ = "MIT"
+
+import os
+from snakemake.shell import shell
+
+revision = snakemake.params.get("revision")
+profile = snakemake.params.get("profile", [])
+config = snakemake.params.get("config")
+resume = snakemake.params.get("resume", True) # TODO: Reasonable default? Conforms nxf to smk...
+process_cache = snakemake.params.get("process_cache", 'lenient') # TODO: Reasonable default? Conforms nxf to smk...
+nxf = snakemake.params.get("nxf", "") # For -log, -w, etc.
+extra = snakemake.params.get("extra", "")
+if isinstance(profile, str):
+    profile = [profile]
+if isinstance(config, str):
+    config = [config]
+
+args = []
+
+if revision:
+    args += ["-revision", revision]
+if profile:
+    args += ["-profile", ",".join(profile)]
+if config:
+    args += ["-config", ",".join(config)]
+if resume:
+    args += ["-resume"]
+if process_cache:
+    args += [f"-process.cache={process_cache}"]
+print(args)
+
+add_parameter = lambda name, value: args.append("--{} {}".format(name, value))
+
+for name, files in snakemake.input.items():
+    if isinstance(files, list):
+        files = ",".join(files)
+    add_parameter(name, files)
+for name, value in snakemake.params.items():
+    if (
+        name != "pipeline"
+        and name != "revision"
+        and name != "profile"
+        and name != "config"
+        and name != "resume"
+        and name != "process_cache"
+        and name != "nxf"
+        and name != "extra"
+    ):
+        add_parameter(name, value)
+
+log = snakemake.log_fmt_shell(stdout=False, stderr=True)
+args = " ".join(args)
+pipeline = snakemake.params.pipeline
+
+shell("nextflow {nxf} run nf-core/{pipeline} {args} {extra} {log}")
